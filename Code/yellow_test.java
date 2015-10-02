1b5a390759a8529733b0cd2d78f24abbecfa7967
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2015-10-02 20:31:26 +0200

    Added function to print the map in the console and implemented a bit more of the mapping function.

--- Code/yellow_test.java
+++ Code/yellow_test.java
@@ -3,10 +3,13 @@ public class Solution {
     int time;
     int shotsFired;
     int[][] map;
+    double danger;
     Position pos;
+     static final int COLS_MAX = 39;
+        static final int ROWS_MAX = 39;
     
     // Used to force the tank to follow a predefined path
-    char[] path = {'P', 'L', 'F', 'R', 'F', 'F', 'F', 'F', 'F', 'F'};
+    char[] path = {'F', 'R', 'F', 'R', 'P', 'P', 'P', 'P', 'P', 'P'};
     
     public enum Direction {
         SOUTH, EAST, NORTH, WEST
@@ -28,13 +31,14 @@ public class Solution {
     }
     
     public Solution() {
-        final int COLS_MAX = 39;
-        final int ROWS_MAX = 39;
+       
         map = new int[ROWS_MAX][COLS_MAX];
     
         pos = new Position();
         shotsFired = 0;
         time = -1;
+        danger = 0;
+        resetMap('_');
         
         /**
          * Currently found targets and corresponding threat level.
@@ -43,7 +47,9 @@ public class Solution {
         //int currentTargets[4];
     }
     
-     // Scans the current surroundings and puts findings in a map. 
+    /**
+     * Scans the current surroundings and puts findings in a map.
+     */
     void mapUpdate(Position p) {
         int distance;
         boolean enemy = false;
@@ -53,7 +59,10 @@ public class Solution {
         }
         switch (p.direction) {
             case SOUTH:
-                // TODO update map
+                if(enemy)
+                    map[pos.row][pos.col + distance] = 'E';
+                else
+                    map[pos.row][pos.col + distance] = 'W';
                 break;
             case EAST:
                 // TODO update map
@@ -62,11 +71,42 @@ public class Solution {
                 // TODO update map
                 break;
             case NORTH:
-                // TODO update map
+                  if(enemy)
+                    map[pos.row][pos.col - distance] = 'E';
+                else
+                    map[pos.row][pos.col - distance] = 'W';
                 break;
         }
     }
 
+    /**
+     * Prints the map in the console window.
+     */
+     void printMap() {
+        for (int r = 0; r < Solution.ROWS_MAX; r++)
+        {
+            for (int c = 0; c < Solution.COLS_MAX; c++)
+            {
+                System.out.printf("%c", map[r][c]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+     }
+     
+    /**
+    * Resets the map by setting every entry to the given char.
+    */
+    void resetMap(char e) {
+        for (int r = 0; r < Solution.ROWS_MAX; r++)
+        {
+            for (int c = 0; c < Solution.COLS_MAX; c++)
+            {
+                map[r][c] = e;
+            }
+        }
+    }
+      
     /**
      * Executes a single step of the tank's programming. The tank can only move,
      * turn, or fire its cannon once per turn. Between each update, the tank's
@@ -75,37 +115,60 @@ public class Solution {
      * runs out of fuel.
      */
     public void update() {
-        mapUpdate(this.pos);
+        if(time < 1) {
+            mapUpdate(this.pos);
+            printMap();
+        }
         time++;
+        System.out.println("\f");
+       
+       /*
         System.out.println("fuel = " + API.currentFuel() + " time " + time);
-        if (8 > time) {
+        if (4 > time) {
             switch (path[time]) {
             case 'R':
+                System.out.println("Right");
                 API.turnRight();
                 break;
                 
             case 'L':
+                System.out.println("Left");
                 API.turnLeft();
                 break;
                 
             case 'F':
+                System.out.println("Forward");
                 API.moveForward();
                 break;
                 
             case 'P':
-                // stand still
+                System.out.println("Pause");
                 break;
                 
+            case 'S':
+                System.out.println("Fire");
+                API.fireCannon();
             default:
                 // do nothing;
             }
         
         }
-       else if (API.identifyTarget()) {
-            System.out.println("Enemy detected");
-            if(2 > shotsFired++)
-            API.fireCannon();
-       }
+     
+       
+        if (API.identifyTarget()) {
+            System.out.println("Enemy!");
+                danger +=1.8;
+        } else {
+            System.out.println("No Enemy detected");
+                if(danger > 0)
+                    danger += -1;
+        }
+        if(danger > 0)
+        API.fireCannon();
+    */
       
     }
 }
+
+
+
